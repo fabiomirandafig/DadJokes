ab40e7c9ae94dfd80871ce4f77ce69bd5c078647
Fábio <fabiomirandafig@gmail.com>
2021-11-10 22:42:12 -0300

    Dad Jokes

---
+++ pages/Jokes/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <SearchJokes @search-text="searchText" />
+    <Joke
+      v-for="joke in jokes"
+      :id="joke.id"
+      :key="joke.id"
+      :joke="joke.joke"
+    />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Joke from '../../components/Joke.vue'
+import SearchJokes from '../../components/SearchJokes.vue'
+
+export default {
+  components: {
+    Joke,
+    SearchJokes
+  },
+  data () {
+    return {
+      jokes: []
+    }
+  },
+  head () {
+    return {
+      title: 'Dad Jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Best place for corny dad jokes'
+        }
+      ]
+    }
+  },
+  async created () {
+    const config = {
+      headers: {
+        Accept: 'application/json'
+      }
+    }
+    try {
+      const res = await axios.get('https://icanhazdadjoke.com/search', config)
+      this.jokes = res.data.results
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  methods: {
+    async searchText (text) {
+      const config = {
+        headers: {
+          Accept: 'application/json'
+        }
+      }
+      try {
+        const res = await axios.get(`https://icanhazdadjoke.com/search?term=${text}`, config)
+        this.jokes = res.data.results
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
